9c4941a144f109064894fdca67c2595ada4eacb8
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-23 16:40:42 +0200

    body of the main

--- lib/main.dart
+++ lib/main.dart
@@ -2,3 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:instigram/responsive.dart/mobile.dart';
 import 'package:instigram/responsive.dart/responsive.dart';
 import 'package:instigram/responsive.dart/web.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+   const MyApp({Key? Key}) : super(key: Key);
+
+   @override
+   Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Responsive(
+        mymobilescreen: MobileScreen(),
+      mywebscreen: Webscreen(),
+      ),
+    );
+   }
+}

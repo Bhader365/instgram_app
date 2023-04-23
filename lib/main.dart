bf82f5612c474dc8a44cdcf41494543a064c3701
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-23 16:39:59 +0200

    call another pages in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,4 @@
 import 'package:flutter/material.dart';
+import 'package:instigram/responsive.dart/mobile.dart';
+import 'package:instigram/responsive.dart/responsive.dart';
+import 'package:instigram/responsive.dart/web.dart';

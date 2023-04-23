2342f741a09f34899676cff25efe3661603476a4
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-23 16:42:29 +0200

    create a class for mobile screen

---
+++ lib/responsive/mobile.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+
+
+class MobileScreen extends StatefulWidget {
+  const MobileScreen({super.key});
+
+  @override
+  State<MobileScreen> createState() => _MobileScreenState();
+}

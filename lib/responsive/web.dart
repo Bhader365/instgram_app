5b69a5dfd2f71a8610a0cf70828fa860a1486952
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-24 02:23:41 +0200

    create a cclass "webscreen"

--- lib/responsive/web.dart
+++ lib/responsive/web.dart
@@ -1,1 +1,8 @@
 import 'package:flutter/material.dart';
+
+class Webscreen extends StatefulWidget {
+  const Webscreen({super.key});
+
+  @override
+  State<Webscreen> createState() => _WebscreenState();
+}

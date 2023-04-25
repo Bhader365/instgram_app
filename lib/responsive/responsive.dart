8982e418959affedcfe0a455fdffa10d2ef5a1d5
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-25 13:23:12 +0200

    body of page responsive

--- lib/responsive/responsive.dart
+++ lib/responsive/responsive.dart
@@ -4,3 +4,24 @@ class Responsive extends StatefulWidget {
     final mymobilescreen;
     final mywebscreen;
   
+  const Responsive({Key? key, required this.mymobilescreen, this.mywebscreen}) : super(key: key);
+
+ @override
+  State<Responsive> createState() => _ResponsiveState();
+}
+
+class _ResponsiveState extends State<Responsive> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder:(BuildContext, BoxConstraints) {
+        if(BoxConstraints.maxWidth > 600) {
+          return widget.mywebscreen;
+        } else {
+          return widget.mymobilescreen;
+        }
+      }
+      
+      );
+  }
+}

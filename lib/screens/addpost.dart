53ebe609c4e84cdb93fa554178c6513317640839
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-27 15:28:39 +0200

    class for add post

--- lib/screens/addpost.dart
+++ lib/screens/addpost.dart
@@ -1,2 +1,9 @@
 import 'package:flutter/material.dart';
 import '../color.dart';
+
+class AddPost extends StatefulWidget {
+  const AddPost({Key? key}) : super(key: key);
+
+  @override
+  State<AddPost> createState() => _AddPostState();
+}

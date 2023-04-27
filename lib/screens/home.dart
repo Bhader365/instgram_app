571430cdff431ddb10f60c80b465e19d6a35eb7c
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-27 15:18:17 +0200

    icon for more

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -74,3 +74,7 @@ class _HomeState extends State<Home> {
                           style: TextStyle(fontSize: 15),
                         ),
                       ]),
+                       IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+                ],
+              ),
+            ),

d14aa8bae7dc877bc1f18d5421e0baac024ce7a7
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-27 15:33:03 +0200

    class of search

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,2 +1,9 @@
 import 'package:flutter/material.dart';
 import '../color.dart';
+
+class Search extends StatefulWidget {
+  const Search({Key? key}) : super(key: key);
+
+  @override
+  State<Search> createState() => _SearchState();
+}

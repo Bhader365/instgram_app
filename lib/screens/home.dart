6400ce01656e13e689e9762f3d81948c6cefc2c8
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-27 15:07:21 +0200

    class of home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 import '../color.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}

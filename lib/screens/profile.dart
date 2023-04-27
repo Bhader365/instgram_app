fca5b658014eab87afaf72db8437d165ec2fe243
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-27 15:42:51 +0200

    varible widthscreen

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -7,3 +7,8 @@ class Profile extends StatefulWidget {
   @override
   State<Profile> createState() => _ProfileState();
 }
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    final double widthScreen = MediaQuery.of(context).size.width;

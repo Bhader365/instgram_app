f5299af3b3f65993e4f370b3800e49e4e7b500e6
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-27 15:27:06 +0200

    margin width some of another details

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -155,3 +155,13 @@ class _HomeState extends State<Home> {
                     textAlign: TextAlign.start,
                   )),
             ),
+             Container(
+                margin: EdgeInsets.fromLTRB(10, 0, 9, 10),
+                width: double.infinity,
+                child: Text(
+                  "10June 2022",
+                  style: TextStyle(
+                      fontSize: 18, color: Color.fromARGB(214, 157, 157, 165)),
+                  textAlign: TextAlign.start,
+                )),
+          ],

885c00dd0b3668b16cc91dc3a0ba612e48710c7d
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-27 15:25:29 +0200

    text "cairo" ,fontsize and color

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -134,3 +134,11 @@ class _HomeState extends State<Home> {
                   style: TextStyle(
                       fontSize: 20, color: Color.fromARGB(255, 189, 196, 199)),
                 ),
+                Text(
+                  " cairo ",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                      fontSize: 18, color: Color.fromARGB(255, 189, 196, 199)),
+                ),
+              ],
+            ),

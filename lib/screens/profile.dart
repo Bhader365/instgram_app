53f22b3c4659188b9de43c131ec4618896c54608
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-27 15:42:21 +0200

    class of profile page

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,2 +1,9 @@
 import 'package:flutter/material.dart';
 import '../color.dart';
+
+class Profile extends StatefulWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}

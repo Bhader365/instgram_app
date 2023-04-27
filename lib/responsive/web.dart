bbc327db892aa878b2973363af311bae3638e3a2
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-27 14:41:11 +0200

    class for web screen

--- lib/responsive/web.dart
+++ lib/responsive/web.dart
@@ -5,3 +5,10 @@ import '../screens/search.dart';
 import '../screens/addpost.dart';
 import '../screens/profile.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+class WebScerren extends StatefulWidget {
+  const WebScerren({Key? key}) : super(key: key);
+
+  @override
+  State<WebScerren> createState() => _WebScerrenState();
+}
